9e1c83b3ee0abb2e47bc320bde876b39b6e4c791
Matthew Bradley <mbradley@cnsonline.net>
2020-04-13 21:38:07 +0100

    changed cardinal to 16 point

--- skins/web-mqtt/update-live-weather-mqtt.js
+++ skins/web-mqtt/update-live-weather-mqtt.js
@@ -5,23 +5,15 @@
 */
 
 
-/*
-* This script connects to an MQTT server to read a 
-*/
-
-
-
-
-
 /** 
- * Given "0-360" returns the nearest cardinal direction "N/NE/E/SE/S/SW/W/NW"  
+  * Given "0-360" returns the nearest cardinal direction "N/NNE/NE/ENE/E/ESE/SE/SSE/S/SSW/SW/WSW/W/WNW/NW/NNW"  
  */
 function getCardinal(angle) {
 	/** 
 	 * Customize by changing the number of directions you have
 	 * We have 8
 	 */
-	const degreePerDirection = 360 / 8;
+	const degreePerDirection = 360 / 16;
   
 	/** 
 	 * Offset the angle by half of the degrees per direction
@@ -30,13 +22,21 @@ function getCardinal(angle) {
 	const offsetAngle = angle + degreePerDirection / 2;
   
 	return (offsetAngle >= 0 * degreePerDirection && offsetAngle < 1 * degreePerDirection) ? "N"
-	  : (offsetAngle >= 1 * degreePerDirection && offsetAngle < 2 * degreePerDirection) ? "NE"
-		: (offsetAngle >= 2 * degreePerDirection && offsetAngle < 3 * degreePerDirection) ? "E"
-		  : (offsetAngle >= 3 * degreePerDirection && offsetAngle < 4 * degreePerDirection) ? "SE"
-			: (offsetAngle >= 4 * degreePerDirection && offsetAngle < 5 * degreePerDirection) ? "S"
-			  : (offsetAngle >= 5 * degreePerDirection && offsetAngle < 6 * degreePerDirection) ? "SW"
-				: (offsetAngle >= 6 * degreePerDirection && offsetAngle < 7 * degreePerDirection) ? "W"
-				  : "NW";
+	  : (offsetAngle >= 1 * degreePerDirection && offsetAngle < 2 * degreePerDirection) ? "NNE"
+		: (offsetAngle >= 2 * degreePerDirection && offsetAngle < 3 * degreePerDirection) ? "NE"
+		  : (offsetAngle >= 3 * degreePerDirection && offsetAngle < 4 * degreePerDirection) ? "ENE"
+			: (offsetAngle >= 4 * degreePerDirection && offsetAngle < 5 * degreePerDirection) ? "E"
+			  : (offsetAngle >= 5 * degreePerDirection && offsetAngle < 6 * degreePerDirection) ? "ESE"
+				: (offsetAngle >= 6 * degreePerDirection && offsetAngle < 7 * degreePerDirection) ? "SE"
+				  : (offsetAngle >= 7 * degreePerDirection && offsetAngle < 8 * degreePerDirection) ? "SSE"
+				    : (offsetAngle >= 8 * degreePerDirection && offsetAngle < 9 * degreePerDirection) ? "S"
+				      : (offsetAngle >= 9 * degreePerDirection && offsetAngle < 10 * degreePerDirection) ? "SSW"
+				        : (offsetAngle >= 10 * degreePerDirection && offsetAngle < 11 * degreePerDirection) ? "SW"
+				          : (offsetAngle >= 11 * degreePerDirection && offsetAngle < 12 * degreePerDirection) ? "WSW"
+				            : (offsetAngle >= 12 * degreePerDirection && offsetAngle < 13 * degreePerDirection) ? "W"
+				              : (offsetAngle >= 13 * degreePerDirection && offsetAngle < 14 * degreePerDirection) ? "WNW"
+				                : (offsetAngle >= 14 * degreePerDirection && offsetAngle < 15 * degreePerDirection) ? "NW"
+				                  : "NNW";
   }
 
 function updateLoopWeather(loopData) {
